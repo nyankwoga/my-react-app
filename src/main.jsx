7a901696e8fc62abb8920c0f918643ba2502f848
nyankwoga <kerubovalentine21@gmail.com>
2025-06-20 10:58:38 +0300

    Added contact me details

--- src/main.jsx
+++ src/main.jsx
@@ -13,5 +13,10 @@ root.render(
       <h1>Weather App</h1>
       <WeatherSearch />
     </div>
+<div className="communication">
+    <a href="https://github.com/nyankwoga/my-react-app" target="_blank" rel="noopener noreferrer">
+  OpenSource
+</a> by <a href="https://github.com/nyankwoga">Valentine Kerubo</a> from <a href="https://www.shecodes.io/ecodes">SheCodes</a>
+</div>
   </StrictMode>
 );
